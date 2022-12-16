c3399f356dbad2aa6addaea6f77108d985004141
abe <abe@lightwits.localdomain>
2022-12-16 13:27:20 +0100

    Expose to internet, randomize port, add rate limiter.
    Note that the rate limiter is not needed if the user is logged in.

--- server-webpack/src/host.ts
+++ server-webpack/src/host.ts
@@ -1,9 +1,30 @@
 import express from 'express';
 import formidable from 'formidable';
+import rateLimit from 'express-rate-limit';
+
+const uploadLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 3,
+  skipSuccessfulRequests: true,  // allow at most 3 failed requests per 10 minutes.
+  standardHeaders: false,
+  legacyHeaders: false,
+  message: 'Too many failed uploads, please try again in 10 minutes.',
+  // NOTE: can set a custom requestWasSuccessful function.
+  // NOTE: can whitelist specific IPs like this:
+  //   skip: (request, response) => ['192.168.0.0'].includes(request.ip),
+})
 
 const app:express.Application = express();
 
-const PORT = 3000;
+const randomRange = (min: number, max: number):number => {
+  console.assert(min < max, 'min must be less than max');
+  console.assert(!Number.isNaN(min) && !Number.isNaN(max),
+    'min and max must be numbers');
+  return min + Math.floor(Math.random() * (max - min));
+};
+
+// Select random open port in [1024, 49152[.
+const PORT = randomRange(1024, 49152);
 
 const dirname = () => {
   return process.cwd() + '/dist';
@@ -13,11 +34,11 @@ app.get('/', (req:any, res:any) => {
   res.sendFile(dirname() + '/fileuploadserver/upload_module.html');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}...`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}...`);
 });
 
-app.post('/api/upload', (req:any, res:any, next:any) => {
+app.post('/api/upload', uploadLimiter, (req:any, res:any, next:any) => {
   const GIBI = 1024 * 1024 * 1024;
   const date = new Date();
   console.log(`${date.toISOString()}_drone_map.tiff`);
